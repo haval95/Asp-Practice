d7a4e7705a5efa6673c933ec5e1a6c9d5bd0da2c
haval95 <40795444+halenkurdi@users.noreply.github.com>
2021-05-27 16:25:06 +0300

    first commit

---
+++ practice/Controllers/ProjectsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        // GET: api/<ProjectsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Get all Projects ");
+        }
+
+        // GET api/<ProjectsController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Reading  project: #{id} ");
+        }
+
+        // POST api/<ProjectsController>
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Creating A Project ");
+        }
+
+        [HttpPut]
+
+        public IActionResult Put()
+        {
+            return Ok("Updating A Projects ");
+        }
+
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            return Ok($"deleting one project: #{id} ");
+        }
+
+        [HttpGet]
+        //here we overwrite the class route by re-initiating the route, BUT we need to have the first / otherwise its going to be added after the class route 
+        [Route("/api/projects/{pid}/tickets")]
+        public IActionResult GetProjectTicket (int pid, int tid)
+        {
+            return Ok($"project number #{pid} and ticket number #{tid}");
+
+        }
+
+
+    }
+}
---
+++ practice/Controllers/TicketsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practice.Controllers
+{
+    //HVL this tells asp this is an api controler and not an mvc controller
+    [ApiController]
+    //we can add [api/[controller]]
+    //using tthis will help Mapcontroller Middleware to find the right controllers based on the URI
+    //Adding the this will help us remove most of the routs that have the controler name
+    [Route("api/[controller]")]
+    //HVL we need to inhirit from controllerBase 
+    public class TicketsController: ControllerBase
+    {
+
+        [HttpGet]
+       // [Route("api/tickets")] we have this if we dont have the route for the class 
+       
+        public IActionResult Get()
+        {
+            return Ok("reading all the tickets" );
+
+        }
+
+        [HttpGet("{id}")]
+       // [Route("api/ticket/{id}")] we would need this without the class route but now we can remove this and the ID to the Http verb attribute 
+        public IActionResult Get(int id)
+        {
+            return Ok($"Reading one ticket: #{id} ");
+        }
+
+        [HttpPost]
+    
+        public IActionResult Post()
+        {
+            return Ok("Creating A ticket ");
+        }
+
+        [HttpPut]
+     
+        public IActionResult Put()
+        {
+            return Ok("Updating A ticket ");
+        }
+
+
+        [HttpDelete("{id}")]
+      
+        public IActionResult Delete(int id )
+        {
+            return Ok($"deleting one ticket: #{id} ");
+        }
+    }
+}
